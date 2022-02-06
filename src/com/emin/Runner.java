8e7ad625f4aed2d2e148fa94852f11781700c609
AimlessCode <emin_baycan@outlook.com>
2022-02-06 13:15:26 +0300

    Initial commit

---
+++ src/com/emin/Runner.java
@@ -0,0 +1,21 @@
+package com.emin;
+
+public class Runner {
+    public static void main(String[] args) {
+        /**
+         * Bir firma ürünlerinin tutacak,3 grup ürünü var.
+         * BireyselUrun, KurumsalUrun, OzelYapimUrun
+         * id,ad,marka,fiyat,stokmiktari,uyarimiktari  alanları ortaktır
+         * Tüm ürünleri için Tek bir SatisRepository Sınıfı vardır . burda
+         * ürünleri göre satış yapılıyor.
+         * Yukarıda ki bilgilere göre
+         * 1- Katıtım yapınız.
+         * 2- Her dınıf ta , default ve parametreli concstructorlar yazınız
+         * 3- Bazı özelliklerin ve methotların public , Private veProtected olası gerekli
+         * 4- Runner da nesne oluşturup , Repository sınıfında satışını yapınız .
+         *
+         *
+         */
+
+    }
+}
